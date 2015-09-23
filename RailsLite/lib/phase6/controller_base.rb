4b8f7be0b0a85f96023acb7148f2bbba751caccb
Garrett Dycus <gjdycus@gmail.com>
2015-09-23 16:08:17 -0700

    Added flash class

--- RailsLite/lib/phase6/controller_base.rb
+++ RailsLite/lib/phase6/controller_base.rb
@@ -2,6 +2,12 @@ require_relative '../phase5/controller_base'
 
 module Phase6
   class ControllerBase < Phase5::ControllerBase
+    attr_accessor :flash
+
+    def initialize(req, res, route_params = {})
+      super(req, res, route_params)
+      @flash = Flash.new
+    end
     # use this with the router to call action_name (:index, :show, :create...)
     def invoke_action(name)
       send(name)
---
+++ RailsLite/lib/phase6/flash.rb
@@ -0,0 +1,25 @@
+class Flash
+  def initialize
+    @messages = Hash.new { Array.new }
+    @now = false
+  end
+
+  def []=(type, message)
+    if @messages[type].empty?
+      @messages[type] = [message]
+    else
+      @messages[type] << message
+    end
+  end
+
+  def [](type)
+    messages = @messages[type]
+    @messages.delete(type)
+    messages
+  end
+
+  def now
+    @now = true
+    self
+  end
+end
